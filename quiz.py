3dd54c0073cdc03af33091ee5cb966d3ff0d4be1
rockwillck <38663064+rockwillck@users.noreply.github.com>
2021-11-18 20:03:29 -0600

    Multiple rounds

--- quiz.py
+++ quiz.py
@@ -6,38 +6,42 @@ terms = f.read()
 f.close()
 
 def gen_quiz(terms, fix:bool=True):
-    if fix:
-        cards = terms.replace("=", "is")
-    else:
-        cards = terms
-    cards = cards.splitlines(False)
-    random.shuffle(cards)
-    while len(cards) > 0:
-        term = cards[0]
-        remWordIndex = random.randint(0, len(term.split(" ")) - 1)
-        termList = term.split(" ")
-        remWord = termList[remWordIndex]
-        termList[remWordIndex] = "_____"
-        termInputStr = ""
-        for el in termList:
-            termInputStr += el + " "
-        termInputStr = termInputStr[:-1]
-        answer = input(termInputStr + ": ")
+    cont = True
+    while cont:
         if fix:
-            truAnswer = re.sub(r'[^a-zA-Z0-9 ]', '', answer)
+            cards = terms.replace("=", "is").replace("&", "and")
         else:
-            truAnswer = answer
-        if fix:
-            truRem = re.sub(r'[^a-zA-Z0-9 ]', '', remWord)
-        else:
-            truRem = remWord
-        if truAnswer.lower() == truRem.lower():
-            print("Correct")
-            cards.remove(term)
-        else:
-            print(f"Incorrect: Correct answer is \"{remWord}\"")
-            cards.remove(term)
-            cards.append(term)
-        print(str(len(cards)) + " terms remaining.")
+            cards = terms
+        cards = cards.splitlines(False)
+        random.shuffle(cards)
+        while len(cards) > 0:
+            term = cards[0]
+            remWordIndex = random.randint(0, len(term.split(" ")) - 1)
+            termList = term.split(" ")
+            remWord = termList[remWordIndex]
+            termList[remWordIndex] = "_____"
+            termInputStr = ""
+            for el in termList:
+                termInputStr += el + " "
+            termInputStr = termInputStr[:-1]
+            answer = input(termInputStr + ": ")
+            if fix:
+                truAnswer = re.sub(r'[^a-zA-Z0-9 ]', '', answer)
+            else:
+                truAnswer = answer
+            if fix:
+                truRem = re.sub(r'[^a-zA-Z0-9 ]', '', remWord)
+            else:
+                truRem = remWord
+            if truAnswer.lower() == truRem.lower():
+                print("Correct")
+                cards.remove(term)
+            else:
+                print(f"Incorrect: Correct answer is \"{remWord}\"")
+                cards.remove(term)
+                cards.append(term)
+            print(str(len(cards)) + " terms remaining.")
+        contInput = input("Play another round? ").lower()
+        cont = False if contInput != "y" and contInput != "yes" else True
 
 gen_quiz(terms)
